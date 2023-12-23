3804bf2436c0d4b2c020438d9e600ea9edb1bcb7
Manoj <balajiimanoj@gmail.com>
2023-12-23 21:30:07 +0530

    Resource addition

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_23_153149) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_23_155429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_img"
+    t.text "thumb_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
