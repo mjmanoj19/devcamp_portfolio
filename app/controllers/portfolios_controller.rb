e6206ba45f2a0e8052f53e72cee7baebaf106039
Manoj <balajiimanoj@gmail.com>
2023-12-24 17:46:02 +0530

    Added delete option

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,7 +43,18 @@ class PortfoliosController < ApplicationController
             
           end
         end
+    end
+
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id])
+        @portfolio_item.destroy 
+        respond_to do |format|
+          format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+          
+        end
       end
+
+      
     
 
 
